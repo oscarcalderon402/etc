21d8565a7f75030477fcfac7bda1e8df03e4a24f
oscar <gregoriocalderon.07@hotmail.es>
2022-06-07 23:23:04 -0400

    setup

---
+++ demos/src/06-setup.test.js
@@ -0,0 +1,36 @@
+describe("set", () => {
+  beforeAll(() => {
+    console.log("beforeAll");
+  });
+
+  afterAll(() => {
+    console.log("after all");
+  });
+
+  beforeEach(() => {
+    console.log("beforeEach");
+  });
+
+  test("case 1", () => {
+    console.log("case 1");
+    expect(1 + 1).toBe(2);
+  });
+  test("case 2", () => {
+    console.log("case 2");
+    expect(1 + 3).toBe(4);
+  });
+
+  describe("other group", () => {
+    beforeAll(() => {
+      console.log("beforeAll");
+    });
+    test("case 3", () => {
+      console.log("case 3");
+      expect(1 + 1).toBe(2);
+    });
+    test("case 4", () => {
+      console.log("case 4");
+      expect(1 + 1).toBe(2);
+    });
+  });
+});
