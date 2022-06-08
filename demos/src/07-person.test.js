d6709aa499b036764d10ef11edb3cfa5aade4385
oscar <gregoriocalderon.07@hotmail.es>
2022-06-07 23:38:56 -0400

    test person

---
+++ demos/src/07-person.test.js
@@ -0,0 +1,22 @@
+const Person = require("./07-person");
+
+describe("Test for person", () => {
+  let person;
+  beforeEach(() => {
+    person = new Person("Oscar", 45, 1.7);
+  });
+
+  test("should return down", () => {
+    person.weight = 45;
+    const imc = person.calcIMC();
+
+    expect(imc).toBe("down");
+  });
+
+  test("should return normal", () => {
+    person.weight = 59;
+    const imc = person.calcIMC();
+
+    expect(imc).toBe("normal");
+  });
+});
