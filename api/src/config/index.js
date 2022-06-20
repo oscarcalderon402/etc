355bb097c42a737e03748b4ef9cf15a954e23a50
oscar <gregoriocalderon.07@hotmail.es>
2022-06-20 15:53:56 -0400

    e2e test

--- api/src/config/index.js
+++ api/src/config/index.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const config = {
-  env: process.env.NODE_ENV || 'dev',
-  isProd: process.env.NODE_ENV === 'production',
+  env: process.env.NODE_ENV || "dev",
+  isProd: process.env.NODE_ENV === "production",
   port: process.env.PORT || 3000,
   dbUrl: process.env.MONGO_URL,
+  dbUrlTest: process.env.MONGO_TEST,
   dbName: process.env.MONGO_DB_NAME,
 };
 
